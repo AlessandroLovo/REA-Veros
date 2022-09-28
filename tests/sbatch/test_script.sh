8eeef89ebcbb7e8773813b78cc76d55cb00ca143
AlessandroLovo <ale571.lovo@gmail.com>
2022-09-28 17:25:01 +0200

    preparing to test sbatch

--- tests/sbatch/test_script.sh
+++ tests/sbatch/test_script.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/bin/bash -l
+#
+#SBATCH -p aegir
+#SBATCH -A ocean
+#SBATCH --job-name=R$1
+#SBATCH --time=00:02:00
+#SBATCH --constraint=v1
+#SBATCH --nodes=1
+
+
+# these options are ignored
+
+#SBATCH --ntasks=12
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+#SBATCH --exclusive
 
 echo "Received argument $1"
 sleep 10
