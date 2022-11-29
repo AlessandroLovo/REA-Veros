ab61f1e7b3ae43c1c941087584eea0d945fe1776
AlessandroLovoENSL <alessandro.lovo@ens-lyon.fr>
2022-11-29 12:32:23 +0100

    test_veros

--- rea/test_veros.sh
+++ rea/test_veros.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. rea.sh -m veros -p test_veros -k 1 -t 5 -e 1 -i 2 -I ../veros/__test__/example.restart.h5 $@
+. rea.sh -m veros -n test_veros -k 1 -t 5 -e 1 -i 2 -I ../veros/__test__/example.restart.h5 $@
