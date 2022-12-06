2422c2e57e1fe963ce47a37dcea9982bd7121c4f
AlessandroLovoENSL <alessandro.lovo@ens-lyon.fr>
2022-12-06 16:17:02 +0100

    Reorganized structure of cluster/

---
+++ cluster/hpc/defaults.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+srun_mpi=true
+partition="aegir"
+account="ocean"
+directives="--constraint=v1"
+dynamics_directives="--exclusive --time=23:59:59"
---
+++ cluster/psmn/defaults.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+srun_mpi=false
+dynamics_directives="--time=12:00:00"
