c6e32011017b7e307fb8d5623e2c17622faae240
AlessandroLovo <ale571.lovo@gmail.com>
2023-03-08 14:16:01 +0100

    not writing on /tmp in test_h5py-mpi.py

--- tests/test_h5py-mpi.py
+++ tests/test_h5py-mpi.py
@@ -1,11 +1,12 @@
 from mpi4py import MPI
 import h5py
+import os
 
 comm = MPI.COMM_WORLD      # Use the world communicator
 mpi_rank = comm.Get_rank() # The process ID (integer 0-41 for a 42-process job)
 mpi_size = comm.Get_size() # Total amount of ranks
 
-destination = '/tmp/test_h5py-mpi_output.h5'
+destination = 'test_h5py-mpi_output.h5'
 
 print(f'Attempting to write to {destination}')
 with h5py.File(destination, 'w', driver='mpio', comm=MPI.COMM_WORLD) as f:
@@ -16,3 +17,6 @@ comm.Barrier()
 
 if (mpi_rank == 0):
     print('42 MPI ranks have finished writing!')
+
+print('Removing output file')
+os.remove(destination)
