cfd95a63a740567995df8cf6e0f40765017c8828
AlessandroLovo <ale571.lovo@gmail.com>
2023-04-12 19:08:36 +0200

    Seems to work

--- rea/test_rea-veros-temp-noise-old.sh
+++ rea/test_rea-veros-temp-noise-old.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./rea.sh -m veros-temp-noise -n test_rea-veros-tn -k 1 -t 5 -e 1 -i 2 -I ../veros-temp-noise-old/__test__/example.restart.h5 "$@"
+. ./rea.sh -m veros-temp-noise-old -n test_rea-veros-tn -k 1 -t 5 -e 1 -i 2 -I ../veros/__test__/example.restart.h5 "$@"
